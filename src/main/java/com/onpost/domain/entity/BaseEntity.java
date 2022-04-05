4a439f0c9cbc44e486dc18f6172d39266928e6e1
cho kyeong hyun <khcho0125@icloud.com>
2022-04-05 21:54:05 +0900

    ⛏️ :: BaseEntity 추가

---
+++ src/main/java/com/onpost/domain/entity/BaseEntity.java
@@ -0,0 +1,18 @@
+package com.onpost.domain.entity;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+    @CreatedDate
+    private LocalDateTime createAt;
+}
