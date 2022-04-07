11085f19805be4e28933dd4a2f93d333c29b9a45
cho kyeong hyun <khcho0125@icloud.com>
2022-04-07 23:07:42 +0900

    ⛏️ :: AuthDetails 추가

---
+++ src/main/java/com/onpost/domain/entity/AuthDetails.java
@@ -0,0 +1,50 @@
+package com.onpost.domain.entity;
+
+import com.onpost.domain.entity.member.Authority;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@AllArgsConstructor
+public class AuthDetails implements UserDetails {
+
+    private String email;
+    private Authority authority;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authority.getAuth();
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
