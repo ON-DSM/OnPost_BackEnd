eb169ae364440843a0f3bb278af6b18c4ff1e5e1
cho kyeong hyun <khcho0125@icloud.com>
2022-04-09 23:06:33 +0900

    ♻️ :: 내부 코드 위치 이동

--- src/main/java/com/onpost/domain/service/PostService.java
+++ src/main/java/com/onpost/domain/service/PostService.java
@@ -2,12 +2,17 @@ package com.onpost.domain.service;
 
 import com.onpost.domain.dto.post.PostRequest;
 import com.onpost.domain.dto.post.PostResponse;
+import com.onpost.domain.entity.Image;
+import com.onpost.domain.entity.Post;
+import com.onpost.domain.entity.member.Member;
+import com.onpost.domain.repository.MemberQueryRepository;
 import com.onpost.domain.repository.PostQueryRepository;
 import com.onpost.global.error.exception.PageSortException;
 import com.querydsl.core.types.OrderSpecifier;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.LinkedHashSet;
 import java.util.List;
 
 import static com.onpost.domain.entity.QPost.post;
@@ -17,17 +22,55 @@ import static com.onpost.domain.entity.QPost.post;
 public class PostService {
 
     private final PostQueryRepository postQueryRepository;
+    private final ImageService imageService;
+    private final MemberQueryRepository memberQueryRepository;
 
-    public PostResponse createPost(PostRequest pcr) {
-        return postQueryRepository.create(pcr);
+
+    public PostResponse createPost(PostRequest postRequest) {
+        List<Image> images = imageService.getImageList(postRequest.getImages(), "static");
+
+        Member writer = memberQueryRepository.getPostAndMember(postRequest.getId());
+
+        Post post = Post.builder()
+                .postLike(new LinkedHashSet<>())
+                .images(images)
+                .context(postRequest.getContext())
+                .title(postRequest.getTitle())
+                .writer(writer)
+                .build();
+
+        writer.updatePost(post);
+
+        postQueryRepository.save(post);
+        memberQueryRepository.save(writer);
+
+        return new PostResponse(post);
     }
 
     public PostResponse showPost(Long id) {
-        return postQueryRepository.show(id);
+        Post find = postQueryRepository.show(id);
+        return new PostResponse(find);
     }
 
     public PostResponse editPost(PostRequest per) {
-        return postQueryRepository.editPost(per);
+        Post find = postQueryRepository.show(per.getId());
+
+        if(per.getContext() != null) {
+            find.setContext(per.getContext());
+        }
+
+        if(per.getTitle() != null) {
+            find.setTitle(per.getTitle());
+        }
+
+        if(per.getImages() != null) {
+            imageService.deleteImageList(find.getImages());
+            find.setImages(imageService.getImageList(per.getImages(), "static"));
+        }
+
+        postQueryRepository.save(find);
+
+        return new PostResponse(find);
     }
 
     public List<PostResponse> pagePost(String sort, Long page) {
@@ -42,8 +85,11 @@ public class PostService {
             default:
                 throw PageSortException.EXCEPTION;
         }
-        return postQueryRepository.showPost(orderBy, page);
+        return postQueryRepository.showPage(orderBy, page);
     }
 
-
+    public void deletePost(Long id) {
+        Post post = postQueryRepository.show(id);
+        postQueryRepository.delete(post);
+    }
 }
