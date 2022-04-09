8d29979aab7a99eb3777d143e72330856cee04aa
cho kyeong hyun <khcho0125@icloud.com>
2022-04-09 23:03:25 +0900

    🔥 :: 필요없는 생성자 제거

--- src/main/java/com/onpost/domain/dto/member/MemberResponse.java
+++ src/main/java/com/onpost/domain/dto/member/MemberResponse.java
@@ -2,7 +2,6 @@ package com.onpost.domain.dto.member;
 
 import com.onpost.domain.entity.member.Authority;
 import com.onpost.domain.entity.member.Member;
-import com.querydsl.core.annotations.QueryProjection;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
@@ -16,20 +15,6 @@ public class MemberResponse extends MemberDto {
     private final Integer follower;
     private final Integer following;
     private final Integer makePost;
-
-    @QueryProjection
-    public MemberResponse(Long id, String name,
-                          String introduce, String profile,
-                          Authority authority, LocalDateTime createAt,
-                          Integer follower, Integer following, Integer makePost) {
-        super(id, name, introduce);
-        this.profile = profile;
-        this.authority = authority;
-        this.createAt = createAt;
-        this.follower = follower;
-        this.following = following;
-        this.makePost = makePost;
-    }
 
     public MemberResponse(Member member) {
         super(member.getId(), member.getName(), member.getIntroduce());
