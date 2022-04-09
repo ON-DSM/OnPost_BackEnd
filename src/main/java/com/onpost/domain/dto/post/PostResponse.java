7ec2e1f2537231cd641dbbd28b0448df6ac2bb9e
cho kyeong hyun <khcho0125@icloud.com>
2022-04-09 23:04:36 +0900

    🔥 :: 필요없는 생성자 제거

--- src/main/java/com/onpost/domain/dto/post/PostResponse.java
+++ src/main/java/com/onpost/domain/dto/post/PostResponse.java
@@ -3,8 +3,6 @@ package com.onpost.domain.dto.post;
 import com.onpost.domain.dto.member.MemberView;
 import com.onpost.domain.entity.Image;
 import com.onpost.domain.entity.Post;
-import com.onpost.domain.entity.member.Member;
-import com.querydsl.core.annotations.QueryProjection;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
@@ -17,15 +15,6 @@ public class PostResponse extends PostDto {
     private final Integer like;
     private final List<Image> images;
     private final LocalDateTime createAt;
-
-    @QueryProjection
-    public PostResponse(Long id, String context, String title, Member writer, Integer like, List<Image> images, LocalDateTime createAt) {
-        super(id, context, title);
-        this.like = like;
-        this.writer = new MemberView(writer);
-        this.images = images;
-        this.createAt = createAt;
-    }
 
     public PostResponse(Post post) {
         super(post.getId(), post.getContext(), post.getTitle());
