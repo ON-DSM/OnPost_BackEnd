ab55ecb5985641b8654e53fd9199f8e42f27c361
cho kyeong hyun <khcho0125@icloud.com>
2022-04-10 16:21:44 +0900

    🔥 :: 문구 수정

--- src/main/java/com/onpost/domain/dto/post/PostRequest.java
+++ src/main/java/com/onpost/domain/dto/post/PostRequest.java
@@ -13,10 +13,10 @@ public class PostRequest extends PostDto {
     private final List<MultipartFile> images;
 
     public PostRequest(@NotBlank(message = "내용이 들어가야합니다!") String context,
-                       @NotNull(message = "작성자 Id가 없습니다!") Long writerId,
+                       @NotNull(message = "작성자 Id가 없습니다!") Long id,
                        @NotBlank(message = "제목이 들어가야합니다!") String title,
                        List<MultipartFile> images) {
-        super(writerId ,context, title);
+        super(id ,context, title);
         this.images = images;
     }
 }
