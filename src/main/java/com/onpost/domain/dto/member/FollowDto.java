6d955f5d35486956b2a9493889b19e784b13716e
cho kyeong hyun <khcho0125@icloud.com>
2022-04-10 16:20:59 +0900

    🔥 :: 문구 수정

--- src/main/java/com/onpost/domain/dto/member/FollowDto.java
+++ src/main/java/com/onpost/domain/dto/member/FollowDto.java
@@ -12,6 +12,6 @@ public class FollowDto {
     @NotNull(message = "제 아이디가 담기지 않았어요!")
     private Long id;
 
-    @NotNull(message = "팔로우할 유저의 Id가 포함되지 않았습니다!")
+    @NotNull(message = "팔로우 유저의 Id가 포함되지 않았습니다!")
     private Long followId;
 }
