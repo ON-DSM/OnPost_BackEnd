77d660c1659541fb9109803613ce0a46b5e9b03b
cho kyeong hyun <khcho0125@icloud.com>
2022-04-11 22:28:07 +0900

    ⛏️ :: 댓글 뷰 추가

---
+++ src/main/java/com/onpost/domain/dto/comment/CommentView.java
@@ -0,0 +1,23 @@
+package com.onpost.domain.dto.comment;
+
+import com.onpost.domain.dto.member.MemberView;
+import com.onpost.domain.entity.comment.Comment;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class CommentView {
+
+    private final Long id;
+    private final String context;
+    private final MemberView writer;
+    private final LocalDateTime createAt;
+
+    public CommentView(Comment comment) {
+        this.id = comment.getId();
+        this.context = comment.getContext();
+        this.createAt = comment.getCreateAt();
+        this.writer = new MemberView(comment.getWriter());
+    }
+}
