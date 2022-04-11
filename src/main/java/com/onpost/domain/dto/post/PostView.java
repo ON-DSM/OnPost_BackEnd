ee1273c14342b18192b111a902a07e91dd2b40c2
cho kyeong hyun <khcho0125@icloud.com>
2022-04-11 22:30:39 +0900

    ⛏️ :: 게시물 하나 뷰 추가

---
+++ src/main/java/com/onpost/domain/dto/post/PostView.java
@@ -0,0 +1,30 @@
+package com.onpost.domain.dto.post;
+
+import com.onpost.domain.dto.comment.MainCommentResponse;
+import com.onpost.domain.dto.member.MemberView;
+import com.onpost.domain.entity.Image;
+import com.onpost.domain.entity.Post;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Getter
+public class PostView extends PostDto {
+
+    private final MemberView writer;
+    private final Integer like;
+    private final List<Image> images;
+    private final LocalDateTime createAt;
+    private final List<MainCommentResponse> comments;
+
+    public PostView(Post post) {
+        super(post.getId(), post.getContext(), post.getTitle());
+        this.writer = new MemberView(post.getWriter());
+        this.createAt = post.getCreateAt();
+        this.like = post.getPostLike().size();
+        this.images = post.getImages();
+        this.comments = post.getComments().stream().map(MainCommentResponse::new).collect(Collectors.toList());
+    }
+}
