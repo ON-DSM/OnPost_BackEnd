2677eb4b0ef88f841113d93a8c4e95de0fa6fb11
cho kyeong hyun <khcho0125@icloud.com>
2022-04-11 22:30:08 +0900

    ♻️ :: Response 수정

--- src/main/java/com/onpost/domain/dto/post/PostResponse.java
+++ src/main/java/com/onpost/domain/dto/post/PostResponse.java
@@ -6,19 +6,18 @@ import com.onpost.domain.entity.Post;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 @Getter
 public class PostResponse extends PostDto {
 
     private final MemberView writer;
     private final Integer like;
-    private final List<Image> images;
+    private final Image image;
     private final LocalDateTime createAt;
 
     public PostResponse(Post post) {
         super(post.getId(), post.getContext(), post.getTitle());
-        this.images = post.getImages();
+        this.image = post.getImages().get(0);
         this.like = post.getPostLike().size();
         this.writer = new MemberView(post.getWriter());
         this.createAt = post.getCreateAt();
