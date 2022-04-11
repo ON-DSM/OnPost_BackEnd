a5ee9db795169403c6c36c0b7a64eb22d30c0e00
cho kyeong hyun <khcho0125@icloud.com>
2022-04-11 22:31:17 +0900

    ⛏️ :: 부모 댓글 추가

---
+++ src/main/java/com/onpost/domain/entity/comment/MainComment.java
@@ -0,0 +1,31 @@
+package com.onpost.domain.entity.comment;
+
+import com.onpost.domain.entity.member.Member;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@DiscriminatorValue("Main")
+@Getter
+public class MainComment extends Comment {
+
+    @OneToMany
+    @JoinTable(
+            name = "child",
+            joinColumns = {@JoinColumn(name = "parent_id")},
+            inverseJoinColumns = {@JoinColumn(name = "child_id")}
+    )
+    private final Set<SubComment> subComments = new LinkedHashSet<>();
+
+    @Builder
+    public MainComment(String context, Member writer) {
+        super(writer, context);
+    }
+}
