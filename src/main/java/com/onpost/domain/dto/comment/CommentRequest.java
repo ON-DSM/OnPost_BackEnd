1133a55197db0d3d1a1fdad9f18b2aba6effd351
cho kyeong hyun <khcho0125@icloud.com>
2022-04-12 19:54:39 +0900

    ⛏️ :: 댓글 Valid 추가

--- src/main/java/com/onpost/domain/dto/comment/CommentRequest.java
+++ src/main/java/com/onpost/domain/dto/comment/CommentRequest.java
@@ -3,10 +3,19 @@ package com.onpost.domain.dto.comment;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 @AllArgsConstructor
 @Getter
 public class CommentRequest {
 
+    @NotBlank(message = "내용이 없습니다!")
     private String context;
+
+    @NotNull(message = "작성자 아이디가 없습니다!")
     private Long writerId;
+
+    @NotNull(message = "어디에 저장해야하는지 모르겠네요")
+    private Long parentId;
 }
