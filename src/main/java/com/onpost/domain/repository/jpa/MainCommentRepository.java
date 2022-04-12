5dd1e63363fde984e7190a896dd9829f0310e6c1
cho kyeong hyun <khcho0125@icloud.com>
2022-04-12 19:54:06 +0900

    ⛏️ :: 댓글 레포 추가

--- src/main/java/com/onpost/domain/repository/jpa/MainCommentRepository.java
+++ src/main/java/com/onpost/domain/repository/jpa/MainCommentRepository.java
@@ -1,9 +1,11 @@
 package com.onpost.domain.repository.jpa;
 
 import com.onpost.domain.entity.comment.Comment;
+import com.onpost.domain.entity.comment.MainComment;
+import org.jboss.jandex.Main;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CommentRepository<T extends Comment> extends JpaRepository<T, Long> {
+public interface CommentRepository extends JpaRepository<MainComment, Long> {
 }
