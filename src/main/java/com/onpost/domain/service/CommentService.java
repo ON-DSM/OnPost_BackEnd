73da600573e0bb48a55514e69ff53fe26bc0ce8f
cho kyeong hyun <khcho0125@icloud.com>
2022-04-12 19:56:59 +0900

    ⛏️ :: 댓글 작업 중

--- src/main/java/com/onpost/domain/service/CommentService.java
+++ src/main/java/com/onpost/domain/service/CommentService.java
@@ -1,29 +1,59 @@
 package com.onpost.domain.service;
 
 import com.onpost.domain.dto.comment.CommentRequest;
+import com.onpost.domain.dto.comment.CommentResponse;
 import com.onpost.domain.dto.comment.CommentView;
-import com.onpost.domain.entity.comment.Comment;
+import com.onpost.domain.entity.Post;
 import com.onpost.domain.entity.comment.MainComment;
+import com.onpost.domain.entity.comment.SubComment;
 import com.onpost.domain.repository.CommentQueryRepository;
 import com.onpost.domain.repository.MemberQueryRepository;
+import com.onpost.domain.repository.PostQueryRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
+@Transactional(rollbackFor = {Exception.class})
 public class CommentService {
 
     private final CommentQueryRepository commentQueryRepository;
     private final MemberQueryRepository memberQueryRepository;
+    private final PostQueryRepository postQueryRepository;
 
-    public CommentView leaveMainComment(CommentRequest commentRequest, Long id) {
-        Comment comment = Comment.builder()
+    public CommentView leaveSubComment(CommentRequest commentRequest) {
+        SubComment comment = SubComment.builder()
                 .context(commentRequest.getContext())
                 .writer(memberQueryRepository.findMember(commentRequest.getWriterId()))
-                ..build();
-        Comment result = commentQueryRepository.leave(comment);
-        return new CommentView(result);
+                .build();
+
+        MainComment parent = commentQueryRepository.findParent(commentRequest.getParentId());
+        parent.getSubComments().add(comment);
+        comment = commentQueryRepository.subLeave(comment);
+
+        commentQueryRepository.mainLeave(parent);
+        return new CommentView(comment);
+    }
+
+    public CommentView leaveComment(CommentRequest commentRequest) {
+        MainComment comment = MainComment.builder()
+                .context(commentRequest.getContext())
+                .writer(memberQueryRepository.findMember(commentRequest.getWriterId()))
+                .build();
+
+        Post post = postQueryRepository.findPostWithComment(commentRequest.getParentId());
+        post.getComments().add(comment);
+        comment = commentQueryRepository.mainLeave(comment);
+
+        postQueryRepository.save(post);
+        return new CommentView(comment);
+    }
+
+    public CommentResponse showMain(Long id) {
+        MainComment comment = commentQueryRepository.findParent(id);
+        return new CommentResponse(comment);
     }
 }
