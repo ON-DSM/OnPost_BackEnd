80d7eb4c0b2cb3647d9fe2b2c58d4a3c085857ba
cho kyeong hyun <khcho0125@icloud.com>
2022-04-12 19:55:23 +0900

    ⛏️ :: 댓글 추가

--- src/main/java/com/onpost/domain/entity/comment/MainComment.java
+++ src/main/java/com/onpost/domain/entity/comment/MainComment.java
@@ -1,10 +1,7 @@
 package com.onpost.domain.entity.comment;
 
 import com.onpost.domain.entity.member.Member;
-import lombok.AccessLevel;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.*;
 import java.util.LinkedHashSet;
