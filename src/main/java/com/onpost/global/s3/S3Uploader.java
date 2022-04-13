52a371c9a887c1ed942f64f9e258d4d63ce6d646
KyungHyun-Cho <82090641+khcho0125@users.noreply.github.com>
2022-04-13 17:30:37 +0900

    🔥 :: 배포 path 긴급 수정

--- src/main/java/com/onpost/global/s3/S3Uploader.java
+++ src/main/java/com/onpost/global/s3/S3Uploader.java
@@ -59,7 +59,7 @@ public class S3Uploader {
     }
 
     private Optional<File> convert(MultipartFile file) throws IOException {
-        File convertFile = new File(System.getProperty("user.dir") + "/src/main/resources/static/" + file.getOriginalFilename());
+        File convertFile = new File(System.getProperty("user.dir") + "/" + file.getOriginalFilename());
         if (convertFile.createNewFile()) {
             try (FileOutputStream fos = new FileOutputStream(convertFile)) {
                 fos.write(file.getBytes());
