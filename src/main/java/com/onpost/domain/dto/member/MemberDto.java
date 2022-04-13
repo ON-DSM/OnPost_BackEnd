1a9d7eb285b7ab59f0e3449d66331aa36a1b98e5
cho kyeong hyun <khcho0125@icloud.com>
2022-04-13 21:44:43 +0900

    ⛏️ :: Valid 추가

--- src/main/java/com/onpost/domain/dto/member/MemberDto.java
+++ src/main/java/com/onpost/domain/dto/member/MemberDto.java
@@ -3,11 +3,20 @@ package com.onpost.domain.dto.member;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 @Getter
 @AllArgsConstructor
 public abstract class MemberDto {
 
+    @NotNull(message = "아이디가 없습니다.")
     private Long id;
+
+    @NotBlank(message = "이름은 6자 이상, 16자 이하이여야 합니다.")
+    @Size(min = 6, max = 16)
     private String name;
+
     private String introduce;
 }
