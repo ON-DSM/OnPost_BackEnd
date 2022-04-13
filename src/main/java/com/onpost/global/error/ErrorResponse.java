9e9d50222ad4bc36fd415ee7c9a643ae7e59fee0
cho kyeong hyun <khcho0125@icloud.com>
2022-04-13 21:47:05 +0900

    ⛏️ :: 생성자 추가

--- src/main/java/com/onpost/global/error/ErrorResponse.java
+++ src/main/java/com/onpost/global/error/ErrorResponse.java
@@ -1,8 +1,10 @@
 package com.onpost.global.error;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class ErrorResponse {
 
     private final String message;
