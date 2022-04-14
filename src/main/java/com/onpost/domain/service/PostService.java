3a06e42e20002de7738b9bd48796e5ee6cf5e428
cho kyeong hyun <khcho0125@icloud.com>
2022-04-14 22:44:11 +0900

    ⛏️ :: Delete 추가

--- src/main/java/com/onpost/domain/service/PostService.java
+++ src/main/java/com/onpost/domain/service/PostService.java
@@ -1,24 +1,28 @@
 package com.onpost.domain.service;
 
-import com.onpost.domain.dto.post.PostDto;
 import com.onpost.domain.dto.post.PostRequest;
 import com.onpost.domain.dto.post.PostResponse;
 import com.onpost.domain.dto.post.PostView;
 import com.onpost.domain.entity.Image;
 import com.onpost.domain.entity.Post;
+import com.onpost.domain.entity.comment.MainComment;
 import com.onpost.domain.entity.member.Member;
+import com.onpost.domain.repository.CommentQueryRepository;
 import com.onpost.domain.repository.MemberQueryRepository;
 import com.onpost.domain.repository.PostQueryRepository;
 import com.onpost.global.error.exception.PageSortException;
 import com.querydsl.core.types.OrderSpecifier;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Set;
 
 import static com.onpost.domain.entity.QPost.post;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 @Transactional(rollbackFor = {Exception.class})
@@ -27,10 +31,11 @@ public class PostService {
     private final PostQueryRepository postQueryRepository;
     private final ImageService imageService;
     private final MemberQueryRepository memberQueryRepository;
+    private final CommentQueryRepository commentQueryRepository;
 
 
     public void createPost(PostRequest postRequest) {
-        List<Image> images = imageService.getImageList(postRequest.getImages(), "static");
+        Set<Image> images = imageService.getImageList(postRequest.getImages(), "static");
 
         Member writer = memberQueryRepository.findOneWithPost(postRequest.getId());
 
@@ -63,7 +68,7 @@ public class PostService {
             find.setTitle(per.getTitle());
         }
 
-        postQueryRepository.deletePostImages(find.getImages());
+        imageService.deleteImageList(find.getImages());
 
         if(per.getImages() != null) {
             find.setImages(imageService.getImageList(per.getImages(), "static"));
@@ -75,25 +80,31 @@ public class PostService {
     }
 
     public List<PostResponse> pagePost(String sort, Long page) {
-        OrderSpecifier<?> orderBy;
-        switch (sort) {
-            case "new":
-                orderBy = post.id.desc();
-                break;
-            case "like":
-                orderBy = post.postLike.size().desc();
-                break;
-            default:
-                throw PageSortException.EXCEPTION;
-        }
+        OrderSpecifier<?> orderBy = switch (sort) {
+            case "new" -> post.id.desc();
+            case "like" -> post.postLike.size().desc();
+            default -> throw PageSortException.EXCEPTION;
+        };
         return postQueryRepository.findPage(orderBy, page);
     }
 
     public void deletePost(Long id) {
-        Post post = postQueryRepository.findPostWithWriter(id);
+        Post post = postQueryRepository.findPostAll(id);
+
         Member member = memberQueryRepository.findOneWithPost(post.getWriter().getId());
         member.deletePost(post);
         memberQueryRepository.save(member);
+
+        Set<Image> dummy = Set.copyOf(post.getImages());
+        post.getImages().clear();
+        imageService.deleteImageList(dummy);
+
+        Set<MainComment> comments = Set.copyOf(post.getComments());
+        post.getComments().clear();
+        comments.forEach(commentQueryRepository::delete);
+
+        post.getPostLike().clear();
+
         postQueryRepository.delete(post);
     }
 }
