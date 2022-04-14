464a5accab505cda90fdd91e978e7f26a841bb4c
cho kyeong hyun <khcho0125@icloud.com>
2022-04-14 22:36:53 +0900

    🔥 :: CommentRepo 통일

--- src/main/java/com/onpost/domain/repository/jpa/MainCommentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.onpost.domain.repository.jpa;
-
-import com.onpost.domain.entity.comment.MainComment;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MainCommentRepository extends JpaRepository<MainComment, Long> {
-}
