6a3dc5b1a8971a23460d33e617805a3cf184c610
cho kyeong hyun <khcho0125@icloud.com>
2022-04-14 22:40:13 +0900

    ⛏️ :: 쿼리 추가

--- src/main/java/com/onpost/domain/repository/CommentQueryRepository.java
+++ src/main/java/com/onpost/domain/repository/CommentQueryRepository.java
@@ -2,9 +2,7 @@ package com.onpost.domain.repository;
 
 import com.onpost.domain.entity.comment.Comment;
 import com.onpost.domain.entity.comment.MainComment;
-import com.onpost.domain.entity.comment.SubComment;
-import com.onpost.domain.repository.jpa.MainCommentRepository;
-import com.onpost.domain.repository.jpa.SubCommentRepository;
+import com.onpost.domain.repository.jpa.CommentRepository;
 import com.onpost.global.error.exception.CommentNotFoundException;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
@@ -12,25 +10,21 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
 
 import static com.onpost.domain.entity.comment.QMainComment.mainComment;
+import static com.onpost.domain.entity.comment.QComment.comment;
 
 @Slf4j
 @Repository
 @RequiredArgsConstructor
 public class CommentQueryRepository {
 
-    private final MainCommentRepository mainCommentRepository;
-    private final SubCommentRepository subCommentRepository;
+    private final CommentRepository commentRepository;
     private final JPAQueryFactory jpaQueryFactory;
 
-    public MainComment mainLeave(MainComment comment) {
-        return mainCommentRepository.save(comment);
+    public Comment leave(Comment comment) {
+        return commentRepository.save(comment);
     }
 
-    public SubComment subLeave(SubComment comment) {
-        return subCommentRepository.save(comment);
-    }
-
-    public MainComment findParent(Long parentId) {
+    public MainComment findMain(Long parentId) {
         MainComment parent = jpaQueryFactory.selectFrom(mainComment)
                 .leftJoin(mainComment.subComments)
                 .fetchJoin()
@@ -38,6 +32,20 @@ public class CommentQueryRepository {
                 .fetchOne();
         check(parent);
         return parent;
+    }
+
+    public Comment findComment(Long id) {
+        Comment find = jpaQueryFactory.selectFrom(comment)
+                .leftJoin(comment.writer)
+                .fetchJoin()
+                .where(comment.id.eq(id))
+                .fetchOne();
+        check(find);
+        return find;
+    }
+
+    public void delete(Comment comment) {
+        commentRepository.delete(comment);
     }
 
     private void check(Comment object) {
