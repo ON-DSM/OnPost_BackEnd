ee5a1380d4a137f6e8447839e39d7df80768578b
cho kyeong hyun <khcho0125@icloud.com>
2022-04-16 23:53:14 +0900

    ⛏️ :: 이메일 인증 체크 추가

--- src/main/java/com/onpost/global/jwt/CustomUserDetailsService.java
+++ src/main/java/com/onpost/global/jwt/CustomUserDetailsService.java
@@ -3,23 +3,24 @@ package com.onpost.global.jwt;
 import com.onpost.domain.entity.AuthDetails;
 import com.onpost.domain.entity.member.Member;
 import com.onpost.domain.repository.jpa.MemberRepository;
+import com.onpost.global.error.exception.EmailCertificationException;
 import com.onpost.global.error.exception.MemberNotFoundException;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 
 @Component
 @Slf4j
-@RequiredArgsConstructor
-public class CustomUserDetailsService {
-
-    private final MemberRepository memberRepository;
+public record CustomUserDetailsService(MemberRepository memberRepository) {
 
     public AuthDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        return memberRepository.findByEmail(email)
-                .map(member -> createUser(email, member)).orElseThrow(() -> MemberNotFoundException.EXCEPTION);
+        Member member = memberRepository.findByEmail(email).orElseThrow(() -> MemberNotFoundException.EXCEPTION);
 
+        if(member.getCertified() != null) {
+            throw EmailCertificationException.EXCEPTION;
+        }
+
+        return createUser(member.getEmail(), member);
     }
 
     private AuthDetails createUser(String email, Member member) {
