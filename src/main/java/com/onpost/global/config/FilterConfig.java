084bfff6d74bf192ed011b526820946257387532
cho kyeong hyun <khcho0125@icloud.com>
2022-04-17 01:03:41 +0900

    ⛏️ :: FilterExceptionHandler 추가

--- src/main/java/com/onpost/global/config/FilterConfig.java
+++ src/main/java/com/onpost/global/config/FilterConfig.java
@@ -1,5 +1,7 @@
 package com.onpost.global.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.onpost.global.error.FilterHandler;
 import com.onpost.global.jwt.JwtFilter;
 import com.onpost.global.jwt.JwtProvider;
 import lombok.RequiredArgsConstructor;
@@ -12,10 +14,13 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class FilterConfig extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
 
     private final JwtProvider jwtProvider;
+    private final ObjectMapper objectMapper;
 
     @Override
     public void configure(HttpSecurity builder) throws Exception {
         JwtFilter customFilter = new JwtFilter(jwtProvider);
+        FilterHandler filterHandler = new FilterHandler(objectMapper);
         builder.addFilterBefore(customFilter, UsernamePasswordAuthenticationFilter.class);
+        builder.addFilterBefore(filterHandler, JwtFilter.class);
     }
 }
