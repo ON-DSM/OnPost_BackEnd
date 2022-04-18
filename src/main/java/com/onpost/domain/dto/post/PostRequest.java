cf035895caaf69f6c617507d4abb37b10143d530
cho kyeong hyun <khcho0125@icloud.com>
2022-04-18 23:47:35 +0900

    🔥 :: 필요없는 구문 제거

--- src/main/java/com/onpost/domain/dto/post/PostRequest.java
+++ src/main/java/com/onpost/domain/dto/post/PostRequest.java
@@ -12,10 +12,7 @@ public class PostRequest extends PostDto {
 
     private final List<MultipartFile> images;
 
-    public PostRequest(@NotBlank(message = "내용이 들어가야합니다!") String context,
-                       @NotNull(message = "작성자 Id가 없습니다!") Long id,
-                       @NotBlank(message = "제목이 들어가야합니다!") String title,
-                       List<MultipartFile> images) {
+    public PostRequest(String context, Long id, String title, List<MultipartFile> images) {
         super(id ,context, title);
         this.images = images;
     }
