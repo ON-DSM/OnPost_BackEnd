4c136e6f860746336df99675b22d0f01810a7413
cho kyeong hyun <khcho0125@icloud.com>
2022-04-18 09:09:41 +0900

    🔥 :: Java mail 제거

--- src/main/java/com/onpost/domain/service/EmailService.java
+++ src/main/java/com/onpost/domain/service/EmailService.java
@@ -4,34 +4,13 @@ import com.amazonaws.AmazonClientException;
 import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
 import com.onpost.domain.dto.SenderDto;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
-
-import javax.mail.MessagingException;
-import javax.mail.internet.MimeMessage;
 
 @Slf4j
 @Service
-public record EmailService(JavaMailSender javaMailSender, AmazonSimpleEmailService amazonSimpleEmailService) {
+public record EmailService(AmazonSimpleEmailService amazonSimpleEmailService) {
 
-    public void sendMail(String toEmail, String subject, String message) {
-        try {
-            MimeMessage mimeMessage = javaMailSender.createMimeMessage();
-            MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "utf-8");
-
-            helper.setFrom("OnPost");
-            helper.setTo(toEmail);
-            helper.setSubject(subject);
-            helper.setText(message, true);
-
-            javaMailSender.send(mimeMessage);
-        } catch (MessagingException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void sendSESMail(SenderDto senderDto) {
+    public void sendMail(SenderDto senderDto) {
         try {
             amazonSimpleEmailService.sendEmail(senderDto.toSendRequestDto());
         } catch (Exception e) {
