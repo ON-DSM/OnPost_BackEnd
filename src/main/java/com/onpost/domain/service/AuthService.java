cfd0bfd8f57c7517ec71ba8674f4d33168c095b9
cho kyeong hyun <khcho0125@icloud.com>
2022-04-21 08:38:41 +0900

    ♻️ :: 트랜잭션 관리

--- src/main/java/com/onpost/domain/service/AuthService.java
+++ src/main/java/com/onpost/domain/service/AuthService.java
@@ -12,15 +12,23 @@ import com.onpost.global.error.exception.EmailAlreadyExistsException;
 import com.onpost.global.error.exception.ExpiredRefreshTokenException;
 import com.onpost.global.error.exception.PasswordNotMatchException;
 import com.onpost.global.jwt.JwtProvider;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import net.bytebuddy.utility.RandomString;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Slf4j
-public record AuthService(JwtProvider jwtProvider, MemberQueryRepository memberQueryRepository,
-                          RefreshRepository refreshRepository, PasswordEncoder passwordEncoder) {
+@RequiredArgsConstructor
+@Transactional(rollbackFor = {Exception.class})
+public class AuthService {
+
+    private final JwtProvider jwtProvider;
+    private final MemberQueryRepository memberQueryRepository;
+    private final RefreshRepository refreshRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public void signupMember(SignupDto signupDto) {
 
