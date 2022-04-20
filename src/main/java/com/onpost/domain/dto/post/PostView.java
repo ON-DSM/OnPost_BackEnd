e977ad59ef537dcff13a719d55800fadf0936f1d
cho kyeong hyun <khcho0125@icloud.com>
2022-04-20 23:48:03 +0900

    ♻️ :: View 수정

--- src/main/java/com/onpost/domain/dto/post/PostView.java
+++ src/main/java/com/onpost/domain/dto/post/PostView.java
@@ -4,27 +4,28 @@ import com.onpost.domain.dto.comment.MainCommentResponse;
 import com.onpost.domain.dto.member.MemberView;
 import com.onpost.domain.entity.Image;
 import com.onpost.domain.entity.Post;
+import com.onpost.domain.entity.comment.MainComment;
+import com.onpost.domain.entity.member.Member;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Getter
 public class PostView extends PostDto {
 
     private final MemberView writer;
-    private final Integer like;
+    private final List<Long> like;
     private final List<Image> images;
     private final LocalDateTime createAt;
     private final List<MainCommentResponse> comments;
 
-    public PostView(Post post) {
+    public PostView(Post post, List<MainComment> comments) {
         super(post.getId(), post.getContext(), post.getTitle());
-        this.writer = new MemberView(post.getWriter());
-        this.createAt = post.getCreateAt();
-        this.like = post.getPostLike().size();
         this.images = post.getImages().stream().toList();
-        this.comments = post.getComments().stream().map(MainCommentResponse::new).collect(Collectors.toList());
+        this.createAt = post.getCreateAt();
+        this.writer = new MemberView(post.getWriter());
+        this.like = post.getPostLike().stream().map(Member::getId).toList();
+        this.comments = comments.stream().map(MainCommentResponse::new).toList();
     }
 }
