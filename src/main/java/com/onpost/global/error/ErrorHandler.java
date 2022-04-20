f8407af5b0800aa2ee6c0801c1056f2b21c741fc
cho kyeong hyun <khcho0125@icloud.com>
2022-04-21 08:40:03 +0900

    ♻️ :: 중복 코드 수정

--- src/main/java/com/onpost/global/error/ErrorHandler.java
+++ src/main/java/com/onpost/global/error/ErrorHandler.java
@@ -2,10 +2,10 @@ package com.onpost.global.error;
 
 import com.onpost.global.error.exception.MasterException;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.tomcat.util.http.fileupload.impl.FileSizeLimitExceededException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindException;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingRequestHeaderException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -18,37 +18,36 @@ public class ErrorHandler {
     @ExceptionHandler(MasterException.class)
     public ResponseEntity<ErrorResponse> masterExceptions(MasterException e) {
         ErrorCode errorCode = e.getErrorCode();
-        log.error("Error status : {}\nError reason : {}", errorCode.getStatus(), errorCode.getMessage());
-        return new ResponseEntity<>(new ErrorResponse(e.getErrorCode()), HttpStatus.valueOf(errorCode.getStatus()));
-    }
-
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ErrorResponse> handleValidationExceptions(MethodArgumentNotValidException e){
-        String[] message = e.getMessage().split("default message");
-        ErrorResponse errorResponse = new ErrorResponse(message[message.length - 1].replaceAll("[\\[|\\]]", "").trim(), 400);
-        log.error("Error status : {}\nError reason : {}", 400, errorResponse.getMessage());
-        return new ResponseEntity<>(errorResponse, HttpStatus.valueOf(errorResponse.getStatus()));
-    }
-
-    @ExceptionHandler(MissingServletRequestParameterException.class)
-    public ResponseEntity<ErrorResponse> handleParameterExceptions(MissingServletRequestParameterException e){
-        ErrorResponse response = new ErrorResponse(ErrorCode.PARAMETER_NOT_CONTAIN);
-        log.error("message : {}, parameterName : {}", e.getMessage(), e.getParameterName());
-        return new ResponseEntity<>(response, HttpStatus.valueOf(response.getStatus()));
-    }
-
-    @ExceptionHandler(MissingRequestHeaderException.class)
-    public ResponseEntity<ErrorResponse> handleHeaderExceptions(MissingRequestHeaderException e) {
-        ErrorResponse response = new ErrorResponse(ErrorCode.HEADER_NOT_CONTAIN);
-        log.error("message : {}, header : {}", e.getMessage(), e.getHeaderName());
-        return new ResponseEntity<>(response, HttpStatus.valueOf(response.getStatus()));
+        return result(new ErrorResponse(errorCode));
     }
 
     @ExceptionHandler(BindException.class)
-    public ResponseEntity<ErrorResponse> handleRequestExceptions(BindException e) {
+    public ResponseEntity<ErrorResponse> handleValidationExceptions(BindException e){
         String[] message = e.getMessage().split("default message");
         ErrorResponse errorResponse = new ErrorResponse(message[message.length - 1].replaceAll("[\\[|\\]]", "").trim(), 400);
-        log.error("Error status : {}\nError reason : {}", 400, errorResponse.getMessage());
-        return new ResponseEntity<>(errorResponse, HttpStatus.valueOf(400));
+        return result(errorResponse);
+    }
+
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    public ResponseEntity<ErrorResponse> handleParameterExceptions(){
+        ErrorResponse errorResponse = new ErrorResponse(ErrorCode.PARAMETER_NOT_CONTAIN);
+        return result(errorResponse);
+    }
+
+    @ExceptionHandler(MissingRequestHeaderException.class)
+    public ResponseEntity<ErrorResponse> handleHeaderExceptions() {
+        ErrorResponse errorResponse = new ErrorResponse(ErrorCode.HEADER_NOT_CONTAIN);
+        return result(errorResponse);
+    }
+
+    @ExceptionHandler(FileSizeLimitExceededException.class)
+    public ResponseEntity<ErrorResponse> handleFileSizeLimitsExceptions(FileSizeLimitExceededException e) {
+        ErrorResponse errorResponse = new ErrorResponse("File size limit : " + e.getFileName(), 413);
+        return result(errorResponse);
+    }
+
+    private ResponseEntity<ErrorResponse> result(ErrorResponse errorResponse) {
+        log.error("Error status : {}\nError reason : {}", errorResponse.getStatus(), errorResponse.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.valueOf(errorResponse.getStatus()));
     }
 }
