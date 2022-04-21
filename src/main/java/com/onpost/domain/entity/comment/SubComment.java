aade97a7700e1a0044c9bf5d54ad4ab634d22277
cho kyeong hyun <khcho0125@icloud.com>
2022-04-21 23:46:01 +0900

    ♻️ :: 관계 필드 추가

--- src/main/java/com/onpost/domain/entity/comment/SubComment.java
+++ src/main/java/com/onpost/domain/entity/comment/SubComment.java
@@ -1,21 +1,23 @@
 package com.onpost.domain.entity.comment;
 
 import com.onpost.domain.entity.member.Member;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
+import lombok.*;
 
-import javax.persistence.DiscriminatorValue;
-import javax.persistence.Entity;
+import javax.persistence.*;
 
 @Getter
 @Entity
-@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @DiscriminatorValue(value = "Sub")
 public class SubComment extends Comment {
 
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "main_comment_id")
+    private MainComment main;
+
     @Builder
-    public SubComment(String context, Member writer, Long parent) {
-        super(writer, context, parent);
+    public SubComment(String content, Member writer, MainComment main) {
+        super(writer, content);
+        this.main = main;
     }
 }
