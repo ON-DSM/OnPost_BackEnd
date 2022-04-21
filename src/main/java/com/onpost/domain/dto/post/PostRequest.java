7cea36ba5475acb04daecf1533df15f534470464
cho kyeong hyun <khcho0125@icloud.com>
2022-04-21 23:43:20 +0900

    ⛏️ :: 게시물 프로필 추가

--- src/main/java/com/onpost/domain/dto/post/PostRequest.java
+++ src/main/java/com/onpost/domain/dto/post/PostRequest.java
@@ -3,17 +3,17 @@ package com.onpost.domain.dto.post;
 import lombok.Getter;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @Getter
 public class PostRequest extends PostDto {
 
     private final List<MultipartFile> images;
+    private final MultipartFile profile;
 
-    public PostRequest(String context, Long id, String title, List<MultipartFile> images) {
-        super(id ,context, title);
+    public PostRequest(String content, Long id, String title, String introduce, MultipartFile profile, List<MultipartFile> images) {
+        super(id ,content, title, introduce);
         this.images = images;
+        this.profile = profile;
     }
 }
