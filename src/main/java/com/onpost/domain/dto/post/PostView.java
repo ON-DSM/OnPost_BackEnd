9db66ab1d3dafe61ce32516f82de5f352d03b144
cho kyeong hyun <khcho0125@icloud.com>
2022-04-21 23:44:30 +0900

    ♻️ :: Field 수정

--- src/main/java/com/onpost/domain/dto/post/PostView.java
+++ src/main/java/com/onpost/domain/dto/post/PostView.java
@@ -14,18 +14,20 @@ import java.util.List;
 @Getter
 public class PostView extends PostDto {
 
+    private final String profile;
     private final MemberView writer;
     private final List<Long> like;
-    private final List<Image> images;
+    private final List<String> images;
     private final LocalDateTime createAt;
     private final List<MainCommentResponse> comments;
 
     public PostView(Post post, List<MainComment> comments) {
-        super(post.getId(), post.getContext(), post.getTitle());
-        this.images = post.getImages().stream().toList();
+        super(post.getId(), post.getContent(), post.getTitle(), post.getIntroduce());
+        this.images = post.getImages().stream().map(Image::getImagePath).toList();
         this.createAt = post.getCreateAt();
         this.writer = new MemberView(post.getWriter());
         this.like = post.getPostLike().stream().map(Member::getId).toList();
         this.comments = comments.stream().map(MainCommentResponse::new).toList();
+        this.profile = post.getProfileImage();
     }
 }
