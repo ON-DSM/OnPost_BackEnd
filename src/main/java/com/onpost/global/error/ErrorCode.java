3d226394a5d821f6488758bf9b5c987e6ec8e444
cho kyeong hyun <khcho0125@icloud.com>
2022-04-22 11:35:06 +0900

    ⛏️ :: Error code 추가

--- src/main/java/com/onpost/global/error/ErrorCode.java
+++ src/main/java/com/onpost/global/error/ErrorCode.java
@@ -7,24 +7,25 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ErrorCode {
 
-    INVALID_TOKEN("Invalid token", 401),
-    EXPIRED_REFRESH_TOKEN("Expired Refresh Token", 401),
-    EMAIL_ALREADY_EXISTS("Email Already Exists", 409),
+    INVALID_TOKEN("Invalid token", "GLOBAL-401-1",401),
+    EXPIRED_REFRESH_TOKEN("Expired Refresh Token", "GLOBAL-401-2", 401),
+    EMAIL_ALREADY_EXISTS("Email Already Exists", "GLOBAL_409-1", 409),
 
-    POST_NOT_FOUND("Post Not Found", 404),
-    IMAGE_NOT_FOUND("Image Not Found", 404),
-    MEMBER_NOT_FOUND("Member Not Found", 404),
-    COMMENT_NOT_FOUND("Comment Not Found", 404),
+    POST_NOT_FOUND("Post Not Found", "POST-404", 404),
+    IMAGE_NOT_FOUND("Image Not Found", "IMAGE-404", 404),
+    MEMBER_NOT_FOUND("Member Not Found", "MEMBER-404",404),
+    COMMENT_NOT_FOUND("Comment Not Found", "COMMENT-404",404),
 
-    FILE_CONVERSION_FAILED("File Change Failed", 400),
+    FILE_CONVERSION_FAILED("File Change Failed","GLOBAL-400-1", 400),
 
-    PARAMETER_NOT_CONTAIN("Parameter Not Contain", 404),
-    HEADER_NOT_CONTAIN("Header Not Contain", 400),
-    PASSWORD_NOT_MATCH("Password Not Match", 401),
-    DO_NOT_UNDERSTAND_SORT("Don't Understand Sort", 404),
+    PARAMETER_NOT_CONTAIN("Parameter Not Contain", "GLOBAL-404-3",404),
+    HEADER_NOT_CONTAIN("Header Not Contain", "GLOBAL-400-2", 400),
+    PASSWORD_NOT_MATCH("Password Not Match",  "GLOBAL-401-4", 401),
+    DO_NOT_UNDERSTAND_SORT("Don't Understand Sort", "GLOBAL-404-4",404),
 
-    NEED_EMAIL_CERTIFICATION("Need Email Certification", 401);
+    NEED_EMAIL_CERTIFICATION("Need Email Certification", "GLOBAL-401-5",401);
 
     private final String message;
+    private final String code;
     private final int status;
 }
