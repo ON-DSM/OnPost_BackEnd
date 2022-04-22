407ef04599492931faa4e73bb586000c2893a1d2
cho kyeong hyun <khcho0125@icloud.com>
2022-04-22 11:34:54 +0900

    ♻️ :: 범위 재조정

--- src/main/java/com/onpost/domain/dto/auth/SignupDto.java
+++ src/main/java/com/onpost/domain/dto/auth/SignupDto.java
@@ -15,8 +15,8 @@ public class SignupDto {
     @Email(message = "이메일 유형이여야 합니다.")
     private String email;
 
-    @NotBlank(message = "이름은 6자 이상, 16자 이하이여야 합니다.")
-    @Size(min = 6, max = 16)
+    @NotBlank(message = "이름은 2자 이상, 6자 이하이여야 합니다.")
+    @Size(min = 2, max = 6)
     private String username;
 
     @NotBlank(message = "패스워드는 8자 이상, 20자 이하여야 합니다.")
