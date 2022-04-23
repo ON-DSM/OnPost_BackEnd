4710f53130a9bef8c07e1a8021a5d0c6124feac9
cho kyeong hyun <khcho0125@icloud.com>
2022-04-24 01:46:55 +0900

    ⛏️ :: NoArgConstructor 추가

--- src/main/java/com/onpost/domain/dto/member/MemberRequest.java
+++ src/main/java/com/onpost/domain/dto/member/MemberRequest.java
@@ -1,12 +1,14 @@
 package com.onpost.domain.dto.member;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.web.multipart.MultipartFile;
 
 @Getter
+@NoArgsConstructor
 public class MemberRequest extends MemberDto {
 
-    private final MultipartFile profile;
+    private MultipartFile profile;
 
     public MemberRequest(Long id, String name,
                          String introduce, MultipartFile profile) {
