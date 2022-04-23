ca0dfd1e4a2c9f7565ff70a68aa249e33ff4adda
cho kyeong hyun <khcho0125@icloud.com>
2022-04-23 22:28:57 +0900

    ♻️ :: validation message 변경

--- src/main/java/com/onpost/domain/dto/member/MemberDto.java
+++ src/main/java/com/onpost/domain/dto/member/MemberDto.java
@@ -11,11 +11,11 @@ import javax.validation.constraints.Size;
 @AllArgsConstructor
 public abstract class MemberDto {
 
-    @NotNull(message = "아이디가 없습니다.")
+    @NotNull(message = "아이디를 찾을 수 없습니다.")
     private Long id;
 
-    @NotBlank(message = "이름은 6자 이상, 16자 이하이여야 합니다.")
-    @Size(min = 6, max = 16)
+    @NotBlank(message = "이름은 2자 이상, 10자 이하이여야 합니다.")
+    @Size(min = 2, max = 10, message = "이름은 2자 이상, 10자 이하이여야 합니다.")
     private String name;
 
     private String introduce;
