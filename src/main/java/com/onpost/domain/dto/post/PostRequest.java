4496a2e3d393d04b495b5bc4b76b77bfeccb34b0
cho kyeong hyun <khcho0125@icloud.com>
2022-04-23 22:30:09 +0900

    ⛏️ :: tag 추가

--- src/main/java/com/onpost/domain/dto/post/PostRequest.java
+++ src/main/java/com/onpost/domain/dto/post/PostRequest.java
@@ -1,19 +1,25 @@
 package com.onpost.domain.dto.post;
 
+import com.onpost.global.error.validation.EditGroup;
 import lombok.Getter;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.validation.constraints.Size;
 import java.util.List;
 
 @Getter
 public class PostRequest extends PostDto {
 
     private final List<MultipartFile> images;
+
+    @Size(max = 200, message = "태그가 너무 길거나 많습니다.", groups = {EditGroup.class})
+    private final String tags;
     private final MultipartFile profile;
 
-    public PostRequest(String content, Long id, String title, String introduce, MultipartFile profile, List<MultipartFile> images) {
+    public PostRequest(String content, Long id, String title, String introduce, MultipartFile profile, List<MultipartFile> images, String tags) {
         super(id ,content, title, introduce);
         this.images = images;
         this.profile = profile;
+        this.tags = tags;
     }
 }
