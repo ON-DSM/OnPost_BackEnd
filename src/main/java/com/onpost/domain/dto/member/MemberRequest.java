94771342fbc9de0c9f9b2ffe7731f049797509c9
cho kyeong hyun <khcho0125@icloud.com>
2022-04-23 22:29:08 +0900

    🔥 :: 필요없는 구문 삭제

--- src/main/java/com/onpost/domain/dto/member/MemberRequest.java
+++ src/main/java/com/onpost/domain/dto/member/MemberRequest.java
@@ -3,15 +3,12 @@ package com.onpost.domain.dto.member;
 import lombok.Getter;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
-
 @Getter
 public class MemberRequest extends MemberDto {
 
     private final MultipartFile profile;
 
-    public MemberRequest(Long id, @NotBlank(message = "이름은 6자 이상, 16자 이하이여야 합니다.") @Size(min = 6, max = 16) String name,
+    public MemberRequest(Long id, String name,
                          String introduce, MultipartFile profile) {
         super(id, name, introduce);
         this.profile = profile;
