9f2b9272fa4d90d029eef6991f7db5bcef82bac2
cho kyeong hyun <khcho0125@icloud.com>
2022-04-24 01:46:38 +0900

    ⛏️ :: NoArgConstructor 추가

--- src/main/java/com/onpost/domain/dto/comment/CommentEditRequest.java
+++ src/main/java/com/onpost/domain/dto/comment/CommentEditRequest.java
@@ -2,17 +2,19 @@ package com.onpost.domain.dto.comment;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
 public class CommentEditRequest {
 
     @NotNull(message = "아이디를 찾을 수 없습니다.")
-    private final Long id;
+    private Long id;
 
     @NotBlank(message = "내용이 없습니다.")
-    private final String context;
+    private String context;
 }
