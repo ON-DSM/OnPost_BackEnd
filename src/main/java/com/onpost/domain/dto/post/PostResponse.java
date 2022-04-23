0981ef8d4c99a12bf4c9added95d7aa4da2b95b6
cho kyeong hyun <khcho0125@icloud.com>
2022-04-23 22:30:23 +0900

    ⛏️ :: QPostResponse 수정

--- src/main/java/com/onpost/domain/dto/post/PostResponse.java
+++ src/main/java/com/onpost/domain/dto/post/PostResponse.java
@@ -1,9 +1,11 @@
 package com.onpost.domain.dto.post;
 
 import com.onpost.domain.dto.member.MemberView;
+import com.querydsl.core.annotations.QueryProjection;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Getter
 public class PostResponse extends PostDto {
@@ -11,15 +13,18 @@ public class PostResponse extends PostDto {
     private final MemberView writer;
     private final Long like;
     private final Long comments;
+    private final List<String> tags;
     private final String profileImage;
     private final LocalDateTime createAt;
 
-    public PostResponse(Long id, String content, String title, String introduce, String profile, Long comments, Long like, MemberView memberView, LocalDateTime createAt) {
+    @QueryProjection
+    public PostResponse(Long id, String content, String title, String introduce, String profile, Long comments, Long like, MemberView memberView, LocalDateTime createAt, String tags) {
         super(id, content, title, introduce);
         this.comments = comments;
         this.like = like;
         this.writer = memberView;
         this.createAt = createAt;
         this.profileImage = profile;
+        this.tags = List.of(tags.split(","));
     }
 }
