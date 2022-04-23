8f7961d2e976b148ac939aa38bafac8e03dd4fa6
cho kyeong hyun <khcho0125@icloud.com>
2022-04-24 01:44:48 +0900

    ⛏️ :: NoArgConstructor 추가

--- src/main/java/com/onpost/domain/dto/auth/SignupDto.java
+++ src/main/java/com/onpost/domain/dto/auth/SignupDto.java
@@ -2,12 +2,14 @@ package com.onpost.domain.dto.auth;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
 @Getter
+@NoArgsConstructor
 @AllArgsConstructor
 public class SignupDto {
 
