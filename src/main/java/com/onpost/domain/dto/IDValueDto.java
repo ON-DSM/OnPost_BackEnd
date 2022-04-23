0f6e63e0cfdc83c5d6c10eca6c13cccc5eb7e187
cho kyeong hyun <khcho0125@icloud.com>
2022-04-24 01:46:43 +0900

    ⛏️ :: NoArgConstructor 추가

--- src/main/java/com/onpost/domain/dto/IDValueDto.java
+++ src/main/java/com/onpost/domain/dto/IDValueDto.java
@@ -2,11 +2,13 @@ package com.onpost.domain.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotNull;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class IDValueDto {
 
     @NotNull(message = "아이디를 찾을 수 없습니다.")
