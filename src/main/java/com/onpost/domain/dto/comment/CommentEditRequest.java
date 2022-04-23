293dc0a21da732b781e9f01548522499d02606bf
cho kyeong hyun <khcho0125@icloud.com>
2022-04-23 22:28:23 +0900

    ♻️ :: validation message 변경

--- src/main/java/com/onpost/domain/dto/comment/CommentEditRequest.java
+++ src/main/java/com/onpost/domain/dto/comment/CommentEditRequest.java
@@ -10,7 +10,7 @@ import javax.validation.constraints.NotNull;
 @Getter
 public class CommentEditRequest {
 
-    @NotNull(message = "아이디를 알 수 없습니다.")
+    @NotNull(message = "아이디를 찾을 수 없습니다.")
     private final Long id;
 
     @NotBlank(message = "내용이 없습니다.")
