f40790d1f907c452a6409af41cf90911378c88b1
cho kyeong hyun <khcho0125@icloud.com>
2022-04-24 01:47:03 +0900

    ⛏️ :: NoArgConstructor 추가

--- src/main/java/com/onpost/domain/dto/post/PostRequest.java
+++ src/main/java/com/onpost/domain/dto/post/PostRequest.java
@@ -2,19 +2,21 @@ package com.onpost.domain.dto.post;
 
 import com.onpost.global.error.validation.EditGroup;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.constraints.Size;
 import java.util.List;
 
 @Getter
+@NoArgsConstructor
 public class PostRequest extends PostDto {
 
-    private final List<MultipartFile> images;
+    private List<MultipartFile> images;
 
     @Size(max = 200, message = "태그가 너무 길거나 많습니다.", groups = {EditGroup.class})
-    private final String tags;
-    private final MultipartFile profile;
+    private String tags;
+    private MultipartFile profile;
 
     public PostRequest(String content, Long id, String title, String introduce, MultipartFile profile, List<MultipartFile> images, String tags) {
         super(id ,content, title, introduce);
