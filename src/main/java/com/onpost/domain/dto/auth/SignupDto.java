81273ebe44bf73cba2532b94357b012ef6de2993
cho kyeong hyun <khcho0125@icloud.com>
2022-04-23 22:28:12 +0900

    ⛏️ :: validation message 추가

--- src/main/java/com/onpost/domain/dto/auth/SignupDto.java
+++ src/main/java/com/onpost/domain/dto/auth/SignupDto.java
@@ -15,11 +15,11 @@ public class SignupDto {
     @Email(message = "이메일 유형이여야 합니다.")
     private String email;
 
-    @NotBlank(message = "이름은 2자 이상, 6자 이하이여야 합니다.")
-    @Size(min = 2, max = 6)
+    @NotBlank(message = "이름은 2자 이상, 10자 이하이여야 합니다.")
+    @Size(min = 2, max = 10, message = "이름은 2자 이상, 10자 이하이여야 합니다.")
     private String username;
 
     @NotBlank(message = "패스워드는 8자 이상, 20자 이하여야 합니다.")
-    @Size(min = 8, max = 20)
+    @Size(min = 8, max = 20, message = "패스워드는 8자 이상, 20자 이하여야 합니다.")
     private String password;
 }
