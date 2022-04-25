25f7888199204d3330acc8553d4b56d15b58490b
cho kyeong hyun <khcho0125@icloud.com>
2022-04-25 16:45:52 +0900

    🔥 :: Constructor 수정

--- src/main/java/com/onpost/domain/dto/member/MemberRequest.java
+++ src/main/java/com/onpost/domain/dto/member/MemberRequest.java
@@ -1,14 +1,12 @@
 package com.onpost.domain.dto.member;
 
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import org.springframework.web.multipart.MultipartFile;
 
 @Getter
-@NoArgsConstructor
 public class MemberRequest extends MemberDto {
 
-    private MultipartFile profile;
+    private final MultipartFile profile;
 
     public MemberRequest(Long id, String name,
                          String introduce, MultipartFile profile) {
