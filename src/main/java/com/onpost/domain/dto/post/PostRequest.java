44e3fba2bd5cd25f1e7404597165c398df4dcad7
cho kyeong hyun <khcho0125@icloud.com>
2022-04-25 16:46:11 +0900

    🔥 :: Constructor 수정

--- src/main/java/com/onpost/domain/dto/post/PostRequest.java
+++ src/main/java/com/onpost/domain/dto/post/PostRequest.java
@@ -9,14 +9,14 @@ import javax.validation.constraints.Size;
 import java.util.List;
 
 @Getter
-@NoArgsConstructor
 public class PostRequest extends PostDto {
 
-    private List<MultipartFile> images;
+    private final List<MultipartFile> images;
 
     @Size(max = 200, message = "태그가 너무 길거나 많습니다.", groups = {EditGroup.class})
-    private String tags;
-    private MultipartFile profile;
+    private final String tags;
+
+    private final MultipartFile profile;
 
     public PostRequest(String content, Long id, String title, String introduce, MultipartFile profile, List<MultipartFile> images, String tags) {
         super(id ,content, title, introduce);
