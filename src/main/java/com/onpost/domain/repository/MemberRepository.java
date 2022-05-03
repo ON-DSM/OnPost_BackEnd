14932e60fecd6dbaa311b760e6f1048fdb56c42c
cho kyeong hyun <khcho0125@icloud.com>
2022-05-03 20:33:56 +0900

    ➕ :: 파일 위치 변경

--- src/main/java/com/onpost/domain/repository/MemberRepository.java
+++ src/main/java/com/onpost/domain/repository/MemberRepository.java
@@ -1,12 +1,13 @@
-package com.onpost.domain.repository.jpa;
+package com.onpost.domain.repository;
 
 import com.onpost.domain.entity.member.Member;
+import com.onpost.domain.repository.Impl.CustomMemberRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface MemberRepository extends JpaRepository<Member, Long> {
+public interface MemberRepository extends JpaRepository<Member, Long>, CustomMemberRepository {
     Optional<Member> findByEmail(String email);
 }
