c7dca4f2e9ee8960ce1921514bcf2dbae2980b5d
cho kyeong hyun <khcho0125@icloud.com>
2022-05-05 01:03:47 +0900

    ⛏️ :: CustomRepo 작성

--- src/main/java/com/onpost/domain/repository/Impl/CommentRepositoryImpl.java
+++ src/main/java/com/onpost/domain/repository/Impl/CommentRepositoryImpl.java
@@ -1,11 +1,11 @@
-package com.onpost.domain.repository.Impl;
+package com.onpost.domain.repository.impl;
 
 import com.onpost.domain.repository.custom.CustomCommentRepository;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
-public class CustomCommentRepositoryImpl implements CustomCommentRepository {
+public class CommentRepositoryImpl implements CustomCommentRepository {
 
     private final JPAQueryFactory jpaQueryFactory;
 
