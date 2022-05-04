95b8fa0fb297a47d378b253c2c1d7fc3b711805d
cho kyeong hyun <khcho0125@icloud.com>
2022-05-04 23:50:37 +0900

    ⛏️ :: 인터페이스 리팩토링

--- src/main/java/com/onpost/domain/repository/PostRepository.java
+++ src/main/java/com/onpost/domain/repository/PostRepository.java
@@ -1,10 +1,29 @@
 package com.onpost.domain.repository;
 
 import com.onpost.domain.entity.Post;
-import com.onpost.domain.repository.Impl.CustomPostRepository;
+import com.onpost.domain.repository.custom.CustomPostRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long>, CustomPostRepository {
+
+    @Query("select p from Post p left join p.images where p.id = :id")
+    Optional<Post> findOneWithImages(@Param("id") Long id);
+
+    @Query("select p from Post p left join p.writer left join p.images left join p.postLike where p.id = :id")
+    Optional<Post> findOneWithAll(@Param("id") Long id);
+
+    @Query("select p from Post p left join p.writer left join p.images left join p.postLike where p.id = :id")
+    Optional<Post> findOneWithWriterAndImagesAndLike(@Param("id") Long id);
+
+    @Query("select p from Post p left join p.comments where p.id = :id")
+    Optional<Post> findOneWithComment(@Param("id") Long id);
+
+    @Query("select p from Post p left join p.postLike where p.id = :id")
+    Optional<Post> findOneWithLike(@Param("id") Long id);
 }
