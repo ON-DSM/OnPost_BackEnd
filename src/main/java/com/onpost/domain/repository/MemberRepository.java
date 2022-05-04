3659ac7af59b7c33f2f45d91b0dbcb9c18b74df4
cho kyeong hyun <khcho0125@icloud.com>
2022-05-04 23:50:32 +0900

    ⛏️ :: 인터페이스 리팩토링

--- src/main/java/com/onpost/domain/repository/MemberRepository.java
+++ src/main/java/com/onpost/domain/repository/MemberRepository.java
@@ -1,8 +1,10 @@
 package com.onpost.domain.repository;
 
 import com.onpost.domain.entity.member.Member;
-import com.onpost.domain.repository.Impl.CustomMemberRepository;
+import com.onpost.domain.repository.custom.CustomMemberRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -10,4 +12,19 @@ import java.util.Optional;
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long>, CustomMemberRepository {
     Optional<Member> findByEmail(String email);
+
+    @Query("select m from Member m left join m.makePost where m.id = :id")
+    Optional<Member> findMemberWithPost(@Param("id") Long id);
+
+    @Query("select m from Member m left join m.follower left join m.following where m.id = :id")
+    Optional<Member> findMemberWithFollow(@Param("id") Long id);
+
+    @Query("select m from Member m left join m.follower left join m.makePost left join m.following where m.id = :id")
+    Optional<Member> findMemberWithAll(@Param("id") Long id);
+
+    @Query("select m from Member m left join m.follower where m.id = :id")
+    Optional<Member> findMemberWithFollower(@Param("id") Long id);
+
+    @Query("select m from Member m left join m.following where m.id = :id")
+    Optional<Member> findMemberWithFollowing(@Param("id") Long id);
 }
