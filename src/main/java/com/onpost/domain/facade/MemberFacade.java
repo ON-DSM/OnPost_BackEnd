875f213ef1f8275b4a59080be732e8bd1b20d45c
cho kyeong hyun <khcho0125@icloud.com>
2022-05-04 23:49:00 +0900

    ⛏️ :: 인터페이스 리팩토링

---
+++ src/main/java/com/onpost/domain/facade/MemberFacade.java
@@ -0,0 +1,48 @@
+package com.onpost.domain.facade;
+
+import com.onpost.domain.entity.member.Member;
+import com.onpost.domain.repository.MemberRepository;
+import com.onpost.global.error.exception.MemberNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class MemberFacade {
+
+    private final MemberRepository memberRepository;
+
+    public Member getInfoMember() {
+        String email = SecurityContextHolder.getContext().getAuthentication().getName();
+        return getMemberByEmail(email);
+    }
+
+    public Member getMemberByEmail(String email) {
+        return memberRepository.findByEmail(email).orElseThrow(() -> MemberNotFoundException.EXCEPTION);
+    }
+
+    public Member getMember(Long id) {
+        return memberRepository.findById(id).orElseThrow(() -> MemberNotFoundException.EXCEPTION);
+    }
+
+    public Member getMemberWithPost(Long id) {
+        return memberRepository.findMemberWithPost(id).orElseThrow(() -> MemberNotFoundException.EXCEPTION);
+    }
+
+    public Member getMemberWithFollow(Long id) {
+        return memberRepository.findMemberWithFollow(id).orElseThrow(() -> MemberNotFoundException.EXCEPTION);
+    }
+
+    public Member getMemberWithFollower(Long id) {
+        return memberRepository.findMemberWithFollower(id).orElseThrow(() -> MemberNotFoundException.EXCEPTION);
+    }
+
+    public Member getMemberWithFollowing(Long id) {
+        return memberRepository.findMemberWithFollowing(id).orElseThrow(() -> MemberNotFoundException.EXCEPTION);
+    }
+
+    public Member getMemberWithAll(Long id) {
+        return memberRepository.findMemberWithAll(id).orElseThrow(() -> MemberNotFoundException.EXCEPTION);
+    }
+}
