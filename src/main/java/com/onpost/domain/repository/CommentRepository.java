56c7f5ec882189f91e0e8f50d5cadd3e8c2d9673
cho kyeong hyun <khcho0125@icloud.com>
2022-05-04 23:49:19 +0900

    ⛏️ :: 인터페이스 리팩토링

--- src/main/java/com/onpost/domain/repository/CommentRepository.java
+++ src/main/java/com/onpost/domain/repository/CommentRepository.java
@@ -1,10 +1,29 @@
 package com.onpost.domain.repository;
 
+import com.onpost.domain.entity.Post;
 import com.onpost.domain.entity.comment.Comment;
-import com.onpost.domain.repository.Impl.CustomCommentRepository;
+import com.onpost.domain.entity.comment.MainComment;
+import com.onpost.domain.entity.member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
-public interface CommentRepository extends JpaRepository<Comment, Long>, CustomCommentRepository {
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    @Query("select m from MainComment m left join m.writer left join m.subComments where m.id = :id")
+    Optional<MainComment> findMainById(@Param("id") Long id);
+
+    @Query("select c from Comment c left join c.writer where c.id = :id")
+    Optional<Comment> findOneById(@Param("id") Long id);
+
+    @Query("select m from MainComment m left join m.subComments where m.parent_post = :post")
+    List<MainComment> findMainByPost(@Param("post") Post post);
+
+    @Query("select c from Comment c where c.writer = :member")
+    List<Comment> findAllByWriter(@Param("member") Member member);
 }
