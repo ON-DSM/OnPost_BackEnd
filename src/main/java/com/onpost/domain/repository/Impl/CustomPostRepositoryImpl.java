712f5015d7ecea7dedd7e5ffbe163ca9e55859bf
cho kyeong hyun <khcho0125@icloud.com>
2022-05-04 23:50:24 +0900

    ♻️ :: 인터페이스 리팩토링

---
+++ src/main/java/com/onpost/domain/repository/Impl/CustomPostRepositoryImpl.java
@@ -0,0 +1,54 @@
+package com.onpost.domain.repository.Impl;
+
+import com.onpost.domain.dto.member.QMemberView;
+import com.onpost.domain.dto.post.PostResponse;
+import com.onpost.domain.dto.post.QPostResponse;
+import com.onpost.domain.entity.Post;
+import com.onpost.domain.entity.Sort;
+import com.onpost.domain.repository.custom.CustomPostRepository;
+import com.onpost.global.error.exception.PageSortException;
+import com.querydsl.core.types.*;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
+
+import java.util.List;
+
+import static com.onpost.domain.entity.QPost.post;
+
+@Slf4j
+public class CustomPostRepositoryImpl extends QuerydslRepositorySupport implements CustomPostRepository {
+
+    private final JPAQueryFactory jpaQueryFactory;
+
+    public CustomPostRepositoryImpl(JPAQueryFactory jpaQueryFactory) {
+        super(Post.class);
+        this.jpaQueryFactory = jpaQueryFactory;
+    }
+
+    @Override
+    public List<PostResponse> findPage(Sort sort, Long page) {
+        return jpaQueryFactory.select(new QPostResponse(
+                        post.id, post.content, post.title, post.introduce, post.profileImage,
+                        post.comments.size().longValue(),
+                        post.postLike.size().longValue(),
+                        new QMemberView(post.writer.id, post.writer.name, post.writer.introduce, post.writer.profile, post.writer.email),
+                        post.createAt,
+                        post.tags
+                ))
+                .from(post)
+                .orderBy(setSort(sort))
+                .limit(16L)
+                .offset((page - 1L) * 16L)
+                .fetch();
+    }
+
+    private OrderSpecifier<?> setSort(Sort sort) {
+        return switch (sort) {
+            case NEW -> post.id.desc();
+            case LIKE -> post.postLike.size().desc();
+            //noinspection UnnecessaryDefault
+            default -> throw PageSortException.EXCEPTION;
+        };
+    }
+}
