1145b97d672a4efccf98c501640d0530d6c0797c
cho kyeong hyun <khcho0125@icloud.com>
2022-05-06 23:40:00 +0900

    ⛏️ :: Follow 분리

--- src/main/java/com/onpost/domain/repository/custom/CustomMemberRepository.java
+++ src/main/java/com/onpost/domain/repository/custom/CustomMemberRepository.java
@@ -1,7 +1,14 @@
 package com.onpost.domain.repository.custom;
 
+import com.onpost.domain.dto.member.MemberView;
+
+import java.util.List;
+
 public interface CustomMemberRepository {
 
     void certified(String email, String certified);
 
+    List<MemberView> searchFollower(Long id);
+
+    List<MemberView> searchFollowing(Long id);
 }
