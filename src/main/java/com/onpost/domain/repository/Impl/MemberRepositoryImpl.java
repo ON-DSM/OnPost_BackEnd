de224183ef4026af9c6a1705b92431e151692eea
cho kyeong hyun <khcho0125@icloud.com>
2022-05-06 23:40:25 +0900

    ⛏️ :: Follow 분리

--- src/main/java/com/onpost/domain/repository/Impl/MemberRepositoryImpl.java
+++ src/main/java/com/onpost/domain/repository/Impl/MemberRepositoryImpl.java
@@ -1,8 +1,13 @@
 package com.onpost.domain.repository.impl;
 
+import com.onpost.domain.dto.member.MemberView;
+import com.onpost.domain.dto.member.QMemberView;
+import com.onpost.domain.entity.member.QMember;
 import com.onpost.domain.repository.custom.CustomMemberRepository;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+
+import java.util.List;
 
 import static com.onpost.domain.entity.member.QMember.member;
 
@@ -11,11 +16,27 @@ public class MemberRepositoryImpl implements CustomMemberRepository {
 
     private final JPAQueryFactory jpaQueryFactory;
 
+    private final static QMember follow = new QMember("member2");
+
     @Override
     public void certified(String email, String certified) {
         jpaQueryFactory.update(member)
                 .setNull(member.certified)
                 .where(member.email.eq(email).and(member.certified.eq(certified)))
                 .execute();
+    }
+
+    @Override
+    public List<MemberView> searchFollower(Long id) {
+        return jpaQueryFactory.select(new QMemberView(
+                follow.id, follow.name, follow.introduce, follow.profile, follow.email
+        )).from(member).join(member.follower, follow).where(member.id.eq(id)).fetch();
+    }
+
+    @Override
+    public List<MemberView> searchFollowing(Long id) {
+        return jpaQueryFactory.select(new QMemberView(
+                follow.id, follow.name, follow.introduce, follow.profile, follow.email
+        )).from(member).join(member.following, follow).where(member.id.eq(id)).fetch();
     }
 }
