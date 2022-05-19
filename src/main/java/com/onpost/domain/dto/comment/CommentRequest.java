232bfe6b1e78526567c78c28089a062b85dca90b
cho kyeong hyun <khcho0125@icloud.com>
2022-05-19 23:07:25 +0900

    ♻️ :: 데이터 불러오기 Id » email 변경

--- src/main/java/com/onpost/domain/dto/comment/CommentRequest.java
+++ src/main/java/com/onpost/domain/dto/comment/CommentRequest.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
@@ -15,8 +16,8 @@ public class CommentRequest {
     @NotBlank(message = "내용이 없습니다!")
     private String context;
 
-    @NotNull(message = "아이디를 찾을 수 없습니다.")
-    private Long writerId;
+    @Email(message = "이메일 형식이여야 합니다.")
+    private String email;
 
     @NotNull(message = "아이디를 찾을 수 없습니다.")
     private Long parentId;
