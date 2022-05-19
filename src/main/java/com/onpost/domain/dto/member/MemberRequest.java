ab817b73b9c115cf188abbacdd53ce6db3cca893
cho kyeong hyun <khcho0125@icloud.com>
2022-05-19 23:09:09 +0900

    ♻️ :: Dto 전체 수정

--- src/main/java/com/onpost/domain/dto/member/MemberRequest.java
+++ src/main/java/com/onpost/domain/dto/member/MemberRequest.java
@@ -1,16 +1,30 @@
 package com.onpost.domain.dto.member;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 @Getter
-public class MemberRequest extends MemberDto {
+@AllArgsConstructor
+@NoArgsConstructor
+public class MemberRequest {
 
-    private final MultipartFile profile;
+    @NotBlank(message = "이름은 2자 이상, 10자 이하이여야 합니다.")
+    @Size(min = 2, max = 10, message = "이름은 2자 이상, 10자 이하이여야 합니다.")
+    private String name;
 
-    public MemberRequest(Long id, String name,
-                         String introduce, MultipartFile profile, boolean public_profile) {
-        super(id, name, introduce, public_profile);
-        this.profile = profile;
-    }
+    @NotBlank(message = "비밀번호는 8자 이상, 20자 이하이여야 합니다.")
+    @Size(min = 8, max = 20, message = "비밀번호는 8자 이상, 20자 이하이여야 합니다.")
+    private String password;
+
+    @Email(message = "이메일 형식이 아닙니다.")
+    private String email;
+
+    private String introduce;
+    private MultipartFile profile;
 }
