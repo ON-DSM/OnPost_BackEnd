4f99c4c24d294ec23ca42685fec5b33ed96be904
cho kyeong hyun <khcho0125@icloud.com>
2022-05-20 10:30:23 +0900

    ♻️ :: Request 이름 변경

--- src/main/java/com/onpost/domain/dto/post/PostEditRequest.java
+++ src/main/java/com/onpost/domain/dto/post/PostEditRequest.java
@@ -12,7 +12,7 @@ import java.util.List;
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
-public class PostRequest {
+public class PostEditRequest {
 
     @NotNull(message = "아이디를 찾을 수 없습니다.")
     private Long id;
