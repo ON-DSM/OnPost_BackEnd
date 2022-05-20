b00c6ce39336baa3096f1f930eaa4c675fe9c3eb
cho kyeong hyun <khcho0125@icloud.com>
2022-05-20 10:32:00 +0900

    ♻️ :: 간단 수정

--- src/main/java/com/onpost/global/fcm/NotificationEvent.java
+++ src/main/java/com/onpost/global/fcm/NotificationEvent.java
@@ -1,14 +1,16 @@
 package com.onpost.global.fcm;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Getter
+@NoArgsConstructor
 public class NotificationEvent {
 
-    private final String message;
-    private final List<String> deviceTokens;
+    private String message;
+    private List<String> deviceTokens;
 
     public NotificationEvent(String message, List<String> deviceTokens) {
         this.message = message;
