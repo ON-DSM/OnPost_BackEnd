322cd2a2f5a157631dde86be871b09014154592d
cho kyeong hyun <khcho0125@icloud.com>
2022-05-20 10:13:27 +0900

    ⛏️ :: DeviceTokenRequest Valid

---
+++ src/main/java/com/onpost/domain/dto/member/MemberDeviceTokenRequest.java
@@ -0,0 +1,20 @@
+package com.onpost.domain.dto.member;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class MemberDeviceTokenRequest {
+
+    @Email(message = "이메일 형식이 아닙니다.")
+    private String email;
+
+    @NotBlank(message = "디바이스 토큰이 없습니다.")
+    private String device_token;
+}
