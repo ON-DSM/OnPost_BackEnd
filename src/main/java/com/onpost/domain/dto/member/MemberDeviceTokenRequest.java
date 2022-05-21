22d1c5f98c530153c5694df7057c524c8876b344
cho kyeong hyun <khcho0125@icloud.com>
2022-05-21 17:25:09 +0900

    🔥 :: NoArgs 삭제

--- src/main/java/com/onpost/domain/dto/member/MemberDeviceTokenRequest.java
+++ src/main/java/com/onpost/domain/dto/member/MemberDeviceTokenRequest.java
@@ -2,13 +2,11 @@ package com.onpost.domain.dto.member;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 
 @AllArgsConstructor
-@NoArgsConstructor
 @Getter
 public class MemberDeviceTokenRequest {
 
