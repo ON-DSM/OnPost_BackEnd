8fd2d9e3672174022b2c19f8b075fec161b7b5fe
cho kyeong hyun <khcho0125@icloud.com>
2022-05-21 17:45:16 +0900

    ⛏️ :: 쿼리 재정의

--- src/main/java/com/onpost/domain/repository/custom/CustomCommentRepository.java
+++ src/main/java/com/onpost/domain/repository/custom/CustomCommentRepository.java
@@ -1,4 +1,11 @@
 package com.onpost.domain.repository.custom;
 
+import com.onpost.domain.dto.comment.MainCommentResponse;
+import com.onpost.domain.entity.Post;
+
+import java.util.List;
+
 public interface CustomCommentRepository {
+
+    List<MainCommentResponse> findMainByPost(Post post);
 }
