9bf44930f8ad82c13527c89af9eecc5e04885f0d
cho kyeong hyun <khcho0125@icloud.com>
2022-05-21 17:46:48 +0900

    ⛏️ :: Security Config

--- src/main/java/com/onpost/global/config/SecurityConfig.java
+++ src/main/java/com/onpost/global/config/SecurityConfig.java
@@ -63,7 +63,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Override
-    public void configure(WebSecurity web) {
+    public void configure(WebSecurity web) throws Exception {
         web.ignoring()
                 .mvcMatchers("/**")
                 .servletPath("/**");
