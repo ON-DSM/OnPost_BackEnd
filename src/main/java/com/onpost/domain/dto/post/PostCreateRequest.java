af460d8f3d80144405f5d9eae903e1ebea4d7ad4
cho kyeong hyun <khcho0125@icloud.com>
2022-05-21 17:26:06 +0900

    ⛏️ :: Valid 추가

--- src/main/java/com/onpost/domain/dto/post/PostCreateRequest.java
+++ src/main/java/com/onpost/domain/dto/post/PostCreateRequest.java
@@ -2,21 +2,26 @@ package com.onpost.domain.dto.post;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 import java.util.List;
 
 @AllArgsConstructor
-@NoArgsConstructor
 @Getter
 public class PostCreateRequest {
 
+    @Email(message = "이메일 유형이여야 합니다.")
     private String email;
-    private String introduce;
+
+    @NotBlank(message = "제목이 없습니다.")
     private String title;
+
+    @NotBlank(message = "내용이 없습니다.")
     private String content;
     private String tags;
+    private String introduce;
     private MultipartFile profile;
     private List<MultipartFile> images;
 }
