3011bb986bc4233f612851caed42038fff615338
cho kyeong hyun <khcho0125@icloud.com>
2022-05-21 17:24:34 +0900

    🔥 :: 상속 해제

--- src/main/java/com/onpost/domain/dto/comment/MainCommentResponse.java
+++ src/main/java/com/onpost/domain/dto/comment/MainCommentResponse.java
@@ -1,15 +1,29 @@
 package com.onpost.domain.dto.comment;
 
+import com.onpost.domain.dto.member.MemberView;
 import com.onpost.domain.entity.comment.MainComment;
+import com.querydsl.core.annotations.QueryProjection;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @Getter
-public class MainCommentResponse extends CommentView {
+@NoArgsConstructor
+public class MainCommentResponse {
 
-    private final Integer moreComment;
+    private Long id;
+    private String content;
+    private MemberView writer;
+    private LocalDateTime createAt;
+    private Integer moreComment;
 
-    public MainCommentResponse(MainComment comment) {
-        super(comment);
-        this.moreComment = comment.getSubComments().size();
+    @QueryProjection
+    public MainCommentResponse(Long id, String content, MemberView writer, LocalDateTime createAt, Integer moreComment) {
+        this.moreComment = moreComment;
+        this.id = id;
+        this.content = content;
+        this.writer = writer;
+        this.createAt = createAt;
     }
 }
