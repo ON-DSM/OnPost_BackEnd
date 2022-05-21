f54e25b38b9f8fe5dd705a47539c06d966a95f4a
cho kyeong hyun <khcho0125@icloud.com>
2022-05-21 17:44:42 +0900

    ♻️ :: builder 구조로 변경

--- src/main/java/com/onpost/domain/dto/post/PostView.java
+++ src/main/java/com/onpost/domain/dto/post/PostView.java
@@ -2,18 +2,16 @@ package com.onpost.domain.dto.post;
 
 import com.onpost.domain.dto.comment.MainCommentResponse;
 import com.onpost.domain.dto.member.MemberView;
-import com.onpost.domain.entity.Image;
-import com.onpost.domain.entity.Post;
-import com.onpost.domain.entity.comment.MainComment;
-import com.onpost.domain.entity.member.Member;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Getter
-@Setter
+@Builder
+@AllArgsConstructor
 public class PostView {
 
     private Long id;
@@ -22,22 +20,9 @@ public class PostView {
     private String introduce;
     private String profile;
     private MemberView writer;
-    private List<Long> like;
+    private List<String> like;
     private List<String> images;
     private List<String> tags;
     private LocalDateTime createAt;
     private List<MainCommentResponse> comments;
-
-    public PostView(Post post, List<MainComment> comments) {
-        this.id = post.getId();
-        this.content = post.getContent();
-        this.title = post.getTitle();
-        this.images = post.getImages().stream().map(Image::getImagePath).toList();
-        this.createAt = post.getCreateAt();
-        this.writer = new MemberView(post.getWriter());
-        this.like = post.getPostLike().stream().map(Member::getId).toList();
-        this.tags = List.of(post.getTags().split(","));
-        this.comments = comments.stream().map(MainCommentResponse::new).toList();
-        this.profile = post.getProfileImage();
-    }
 }
