ba71cfb01c5b66bfb5860a6417196bac5bc8137c
cho kyeong hyun <khcho0125@icloud.com>
2022-05-21 17:23:50 +0900

    ⛏️ :: NoArgs 추가

--- src/main/java/com/onpost/domain/dto/comment/CommentView.java
+++ src/main/java/com/onpost/domain/dto/comment/CommentView.java
@@ -3,20 +3,22 @@ package com.onpost.domain.dto.comment;
 import com.onpost.domain.dto.member.MemberView;
 import com.onpost.domain.entity.comment.Comment;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Getter
+@NoArgsConstructor
 public class CommentView {
 
-    private final Long id;
-    private final String context;
-    private final MemberView writer;
-    private final LocalDateTime createAt;
+    private Long id;
+    private String content;
+    private MemberView writer;
+    private LocalDateTime createAt;
 
     public CommentView(Comment comment) {
         this.id = comment.getId();
-        this.context = comment.getContent();
+        this.content = comment.getContent();
         this.createAt = comment.getCreateAt();
         this.writer = new MemberView(comment.getWriter());
     }
