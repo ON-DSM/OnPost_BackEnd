1b18245b5afe87693bd407a1dfc126e4028476ed
cho kyeong hyun <khcho0125@icloud.com>
2022-05-21 17:45:03 +0900

    🔥 :: 쿼리 삭제

--- src/main/java/com/onpost/domain/repository/CommentRepository.java
+++ src/main/java/com/onpost/domain/repository/CommentRepository.java
@@ -1,6 +1,5 @@
 package com.onpost.domain.repository;
 
-import com.onpost.domain.entity.Post;
 import com.onpost.domain.entity.comment.Comment;
 import com.onpost.domain.entity.comment.MainComment;
 import com.onpost.domain.entity.member.Member;
@@ -10,7 +9,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -21,9 +19,6 @@ public interface CommentRepository extends JpaRepository<Comment, Long>, CustomC
 
     @Query("select c from Comment c left join c.writer where c.id = :id")
     Optional<Comment> findOneById(@Param("id") Long id);
-
-    @Query("select m from MainComment m left join m.subComments where m.parent_post = :post")
-    List<MainComment> findMainByPost(@Param("post") Post post);
 
     void deleteCommentByWriter(Member writer);
 }
