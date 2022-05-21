cafd35640f38e752914723a95ba70f4a6e676634
cho kyeong hyun <khcho0125@icloud.com>
2022-05-21 17:47:00 +0900

    ➕ :: 알림 객체

---
+++ src/main/java/com/onpost/global/fcm/NotificationMessage.java
@@ -0,0 +1,25 @@
+package com.onpost.global.fcm;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+//@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+public class NotificationMessage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String message;
+}
