b03cc33ad622f9fc77de29324a57cb262fa62a36
cho kyeong hyun <khcho0125@icloud.com>
2022-06-08 20:26:21 +0900

    ♻️ :: 이미지 리팩토링

--- src/main/java/com/onpost/domain/service/ImageService.java
+++ src/main/java/com/onpost/domain/service/ImageService.java
@@ -1,39 +1,22 @@
 package com.onpost.domain.service;
 
 import com.onpost.domain.entity.Image;
-import com.onpost.domain.entity.Post;
 import com.onpost.domain.repository.ImageRepository;
 import com.onpost.global.aws.S3Uploader;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.*;
-
 @Service
 public record ImageService(S3Uploader s3Uploader, ImageRepository imageRepository) {
 
-    public void deleteImageList(Set<Image> images) {
-        for (Image image : images) {
-            String[] paths = image.getImagePath().split("/");
-            s3Uploader.delete(paths[paths.length - 2] + "/" + paths[paths.length - 1]);
-            imageRepository.delete(image);
-        }
-    }
-
-    public void addImageList(List<MultipartFile> images, String dirName, Post post) {
-        Set<Image> imageList = new LinkedHashSet<>();
-        if (images != null) {
-            for (MultipartFile im : images) {
-                imageList.add(Image.builder()
-                        .usingPost(post)
-                        .imagePath(s3Uploader.upload(im, dirName)).build());
-            }
-        }
-        post.setImages(imageList);
+    public String addImage(MultipartFile image, String dirName) {
+        return s3Uploader.upload(image, dirName);
     }
 
     public String getPath(MultipartFile file, String dirName) {
-        return s3Uploader.upload(file, dirName);
+        Image image = Image.builder().imagePath(s3Uploader.upload(file, dirName)).build();
+        imageRepository.save(image);
+        return image.getImagePath();
     }
 
     public void deletePath(String path) {
