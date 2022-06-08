6858bd7d6ad0d1abbbfa6180d8fcbe6ccfae0471
cho kyeong hyun <khcho0125@icloud.com>
2022-06-08 20:26:16 +0900

    ♻️ :: 이미지 리팩토링

--- src/main/java/com/onpost/domain/repository/PostRepository.java
+++ src/main/java/com/onpost/domain/repository/PostRepository.java
@@ -12,14 +12,11 @@ import java.util.Optional;
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long>, CustomPostRepository {
 
-    @Query("select p from Post p left join p.images where p.id = :id")
-    Optional<Post> findOneWithImages(@Param("id") Long id);
+    @Query("select p from Post p where p.id = :id")
+    Optional<Post> findOne(@Param("id") Long id);
 
-    @Query("select p from Post p left join p.writer left join p.images left join p.postLike where p.id = :id")
+    @Query("select p from Post p left join p.writer left join p.postLike where p.id = :id")
     Optional<Post> findOneWithAll(@Param("id") Long id);
-
-    @Query("select p from Post p left join p.writer left join p.images left join p.postLike where p.id = :id")
-    Optional<Post> findOneWithWriterAndImagesAndLike(@Param("id") Long id);
 
     @Query("select p from Post p left join p.comments where p.id = :id")
     Optional<Post> findOneWithComment(@Param("id") Long id);
