30049004a3652ed7522006358a18467e856d55a6
cho kyeong hyun <khcho0125@icloud.com>
2022-06-09 19:49:51 +0900

    🔥 :: undo

--- src/main/java/com/onpost/global/config/SecurityConfig.java
+++ src/main/java/com/onpost/global/config/SecurityConfig.java
@@ -56,7 +56,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/post/show/**").permitAll()
                 .antMatchers("/mail/**").permitAll()
                 .antMatchers("/member/profile/**").permitAll()
-                .antMatchers("/member/info").permitAll()
                 .antMatchers("/member/following").permitAll()
                 .antMatchers("/member/followers").permitAll()
                 .antMatchers("/post/search").permitAll()
