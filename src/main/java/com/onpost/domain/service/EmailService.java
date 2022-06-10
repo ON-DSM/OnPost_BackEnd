59323d99bfed3db72a3cf06e128dcf9ed03ca102
cho kyeong hyun <khcho0125@icloud.com>
2022-06-10 17:12:54 +0900

    ♻️ :: ses template 도입

--- src/main/java/com/onpost/domain/service/EmailService.java
+++ src/main/java/com/onpost/domain/service/EmailService.java
@@ -2,6 +2,7 @@ package com.onpost.domain.service;
 
 import com.amazonaws.AmazonClientException;
 import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
+import com.amazonaws.services.simpleemail.model.SendTemplatedEmailRequest;
 import com.onpost.domain.dto.SenderDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -10,9 +11,9 @@ import org.springframework.stereotype.Service;
 @Service
 public record EmailService(AmazonSimpleEmailService amazonSimpleEmailService) {
 
-    public void sendMail(SenderDto senderDto) {
+    public void sendMail(SendTemplatedEmailRequest request) {
         try {
-            amazonSimpleEmailService.sendEmail(senderDto.toSendRequestDto());
+            amazonSimpleEmailService.sendTemplatedEmail(request);
         } catch (Exception e) {
             log.error("Error Message: " + e.getMessage());
             throw new AmazonClientException(e.getMessage(), e);
