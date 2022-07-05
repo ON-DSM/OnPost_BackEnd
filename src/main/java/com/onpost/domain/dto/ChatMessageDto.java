57e6968ba4816805d5697c3f4156729e366a1749
cho kyeong hyun <khcho0125@icloud.com>
2022-07-05 15:03:30 +0900

    ⛏️ :: NoArgsConstructor

--- src/main/java/com/onpost/domain/dto/ChatMessageDto.java
+++ src/main/java/com/onpost/domain/dto/ChatMessageDto.java
@@ -2,9 +2,11 @@ package com.onpost.domain.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class ChatMessageDto {
 
     private String message;
