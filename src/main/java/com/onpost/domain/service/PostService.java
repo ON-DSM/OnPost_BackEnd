c8b9a9dd52e5f130fe6db045006e9cc98ef8f7dd
Jo Kyung Hyeon <khcho0125@icloud.com>
2022-07-10 22:31:54 +0900

    ♻️ :: transactional 어노테이션 필수

--- src/main/java/com/onpost/domain/service/PostService.java
+++ src/main/java/com/onpost/domain/service/PostService.java
@@ -15,12 +15,14 @@ import com.onpost.global.error.exception.InvalidTokenException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
 @Service
+@Transactional(rollbackFor = {Exception.class})
 @RequiredArgsConstructor
 public class PostService {
 
